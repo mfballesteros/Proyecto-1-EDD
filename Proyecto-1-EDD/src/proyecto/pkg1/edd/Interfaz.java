ce97fc70ee28a52b8d92b3439eccaa80fac947c1
mfballesteros <m.ballesteros@correo.unimet.edu.ve>
2025-10-27 19:15:37 -0400

    Funcionamiento del JFileChooser y modificacion de interfaz

--- Proyecto-1-EDD/src/proyecto/pkg1/edd/Interfaz.java
+++ Proyecto-1-EDD/src/proyecto/pkg1/edd/Interfaz.java
@@ -4,6 +4,10 @@
  */
 package proyecto.pkg1.edd;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 /**
  *
  * @author a-utr
@@ -27,32 +31,70 @@ public class Interfaz extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        botoncargararchivo = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(0, 7), new java.awt.Dimension(0, 7), new java.awt.Dimension(32767, 7));
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jTextField2 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setText("¡Bienvenido! ");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 30, -1, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 20, -1, -1));
 
-        jButton1.setText("Cargar Archivo 📁");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        botoncargararchivo.setText("Cargar Archivo 📁");
+        botoncargararchivo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                botoncargararchivoActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 73, -1, 30));
+        getContentPane().add(botoncargararchivo, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 60, -1, 30));
 
-        jLabel2.setText("Para iniciar agregue el archivo que desea analizar   ➡️ ");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, -1, 30));
+        jLabel2.setText("Para iniciar agregue el archivo que desea analizar   ️➡");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, -1, 30));
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 120, 390, 30));
+        getContentPane().add(filler1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 200, -1, -1));
+
+        jButton1.setText("Agregar Relación");
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 430, -1, -1));
+
+        jButton2.setText("Agregar Usuario");
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 430, -1, -1));
+
+        jButton4.setText("Crear Grafo");
+        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 430, -1, -1));
+        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 180, 390, 220));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void botoncargararchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botoncargararchivoActionPerformed
+        JFileChooser chooser =new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos de Texto(.txt)","txt");
+        chooser.setFileFilter(filter);
+        chooser.setAcceptAllFileFilterUsed(false);
+        chooser.showOpenDialog(null);
+        File f =chooser.getSelectedFile();
+        
+        if (f != null){
+            String filename=f.getAbsolutePath();
+            jTextField1.setText(filename);
+        }  
+    }//GEN-LAST:event_botoncargararchivoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -90,8 +132,14 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botoncargararchivo;
+    private javax.swing.Box.Filler filler1;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
