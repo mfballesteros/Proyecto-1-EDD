9a2d66e19e34e8b018db7e27828b0282be10f7c6
bcarnevali <bcarnevali@correo.unimet.edu.ve>
2025-10-28 15:25:34 -0400

    Actualizacion de rama remota con info nueva}

--- Proyecto-1-EDD/src/proyecto/pkg1/edd/Proyecto1EDD.java
+++ Proyecto-1-EDD/src/proyecto/pkg1/edd/Proyecto1EDD.java
@@ -14,7 +14,18 @@ public class Proyecto1EDD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ListaAdyacencia Lista1 = new ListaAdyacencia();
+        Lista1.insertarPrincipio (2);
+        Lista1.insertarFinal(3);
+        Lista1.mostrarLista();
+        Lista1.insertarPrincipio(4);
+       
+        Lista1.insertarFinal(7);
+        Lista1.eliminarFinal();
+        Lista1.mostrarLista();
+        
+        
+       
     }
     
 }
